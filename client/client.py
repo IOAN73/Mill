e64c20edcf5b97159177f941b6979b025ee27cee
Raidzin <60663550+Raidzin@users.noreply.github.com>
2024-01-23 21:26:00 +0300

    update client

--- client/client.py
+++ client/client.py
@@ -1,47 +1,15 @@
-from time import sleep
-
 from httpx import Client
+from server.schemas import Game
 
 URL = 'http://localhost:8000/game'
 
 
-def main():
+def get_game() -> Game:
     with Client() as client:
-        while True:
-            # response = client.get(URL)
-            # data = response.json()
-            # sleep(2)
-            response = client.post(
-                url=URL,
-                json={
-                    'from_position': (1, 2),
-                    'to_position': (2, 2),
-                }
-            )
-            print(response.status_code)
+        response = client.get(URL)
+        return Game(**response.json())
 
 
-def get_positions():
-    # with Client() as client:
-    #     response = client.get(URL)
-    #     if response.status_code != 200:
-    #         raise ValueError
-    #     return response.json()
-    return {
-        "tricks_positions": [
-            {
-                "color": "white",
-                "position": [1, 2]
-            },
-            {
-                "color": "black",
-                "position": [2, 2]
-            },
-        ],
-        "tricks_count": 0
-    }
-
-def send_positions(...):
-    pass
-
-def delete_trick(): ...
+if __name__ == '__main__':
+    game = get_game()
+    print(game)
