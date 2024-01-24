df4233eefd11c14a35daca6a6a660621b03f548d
Raidzin <60663550+Raidzin@users.noreply.github.com>
2024-01-24 19:07:14 +0300

    add base desk

--- server/schemas.py
+++ server/schemas.py
@@ -79,3 +79,13 @@ class Game(BaseModel):
 class Movement(BaseModel):
     from_position: Position
     to_position: Position
+
+
+desk: dict[Position, list[Position]] = {
+    (0, 0): [(3, 0), (0, 3)],
+    (3, 0): [(0, 0), (3, 1), (6, 0)],
+    (6, 0): [(3, 0), (6, 3)],
+    (1, 1): [(1, 3), (3, 1)],
+    (3, 1): [(1, 1), (3, 0), (3, 2), (5, 1)],
+    ...
+}
