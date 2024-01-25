6061af0935c71fb04f7a5b448ae4e1ef4a2a1ac6
ichap <i.chapygin71@gmail.com>
2024-01-25 18:49:30 +0300

    popravleni_pos

--- client/part2.py
+++ client/part2.py
@@ -1,18 +1,21 @@
 import pygame
 import sys
-import random
+
+from server.schemas import Trick, Color
 
 # Инициализация Pygame
 pygame.init()
 
 # Загрузка изображений
-game_board_image = pygame.image.load('../static/pole.jpg')
-black_piece_image = pygame.image.load('../static/black.png')
-white_piece_image = pygame.image.load('../static/white.png')
+game_board_image = pygame.image.load('C:/Python_Projects/Mill/static/pole.jpg')
+black_piece_image = pygame.image.load('C:/Python_Projects/Mill/static/black.png')
+white_piece_image = pygame.image.load('C:/Python_Projects/Mill/static/white.png')
 
 # Определение цветов
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+
+
 
 # Установка размеров экрана
 WIDTH, HEIGHT = game_board_image.get_width(), game_board_image.get_height()
@@ -55,8 +58,36 @@ def handle_start_screen_click(mouse_position, two_players_button, vs_computer_bu
         return None
 
 
+
+# Добавление координат точек на игровом поле
+points = {'A1': (30, 670), 'A4': (30, 350), 'A7': (30, 30),
+          'B2': (135, 560), 'B4': (135, 350), 'B6': (135, 140),
+          'C3': (240, 450), 'C4': (240, 350), 'C5': (240, 240),
+          'D1': (350, 670), 'D2': (350, 560), 'D3': (350, 450), 'D5': (350, 240), 'D6': (350, 140), 'D7': (350, 30),
+          'E3': (450, 450), 'E4': (450, 350), 'E5': (450, 240),
+          'F2': (560, 560), 'F4': (560, 350), 'F6': (560, 140),
+          'G1': (670, 670), 'G4': (670, 350), 'G7': (670, 30)}
+
 # Расположение фишек
 pieces = {'A1': None, 'A4': None, 'A7': None,
+          'B2': None, 'B4': None, 'B6': None,
+          'C3': None, 'C4': None, 'C5': None,
+          'D1': None, 'D2': None, 'D3': None, 'D5': None, 'D6': None, 'D7': None,
+          'E3': None, 'E4': None, 'E5': None,
+          'F2': None, 'F4': None, 'F6': None,
+          'G1': None, 'G4': None, 'G7': None}
+
+# Расположение фишек black
+black_pieces = {'A1': None, 'A4': None, 'A7': None,
+          'B2': None, 'B4': None, 'B6': None,
+          'C3': None, 'C4': None, 'C5': None,
+          'D1': None, 'D2': None, 'D3': None, 'D5': None, 'D6': None, 'D7': None,
+          'E3': None, 'E4': None, 'E5': None,
+          'F2': None, 'F4': None, 'F6': None,
+          'G1': None, 'G4': None, 'G7': None}
+
+# Расположение фишек white
+white_pieces = {'A1': None, 'A4': None, 'A7': None,
           'B2': None, 'B4': None, 'B6': None,
           'C3': None, 'C4': None, 'C5': None,
           'D1': None, 'D2': None, 'D3': None, 'D5': None, 'D6': None, 'D7': None,
@@ -69,209 +100,154 @@ selected_piece = None
 turn = 'black'
 
 
-# Функция для проверки победы
-def check_win():
-    lines = [
-        ('A1', 'A4', 'A7'), ('B2', 'B4', 'B6'), ('C3', 'C4', 'C5'),
-        ('D1', 'D2', 'D3'), ('D5', 'D6', 'D7'), ('E3', 'E4', 'E5'),
-        ('F2', 'F4', 'F6'), ('G1', 'G4', 'G7'),
-        ('A1', 'D1', 'G1'), ('B2', 'D2', 'F2'), ('C3', 'D3', 'E3'),
-        ('A4', 'B4', 'C4'), ('E4', 'F4', 'G4'), ('C5', 'D5', 'E5'),
-        ('B6', 'D6', 'F6'), ('A7', 'D7', 'G7')
-    ]
-    for line in lines:
-        if pieces[line[0]] == pieces[line[1]] == pieces[line[2]] and pieces[line[0]] is not None:
-            return True
-    return False
-
-
-# Рисование игрового поля и фишек
-def draw_board():
-    screen.blit(game_board_image, (0, 0))
-    for position, pixel_position in pieces.items():
-        if pieces[position] == 'black':
-            screen.blit(black_piece_image, pixel_position)
-        elif pieces[position] == 'white':
-            screen.blit(white_piece_image, pixel_position)
+# Функция отрисовки точек
+def draw_points():
+    for pos, (x, y) in points.items():
+        pygame.draw.circle(screen, BLACK, (x, y), 10)
+        font = pygame.font.Font(None, 24)
+        text = font.render(pos, True, BLACK)
+        text_rect = text.get_rect(center=(x, y))
+        screen.blit(text, text_rect)
 
 
 # Преобразование позиции в координаты на экране
 def position_to_pixel(position):
-    return pieces[position]
+    letters = 'ABCDEFG'
+    x = (letters.index(position[0]) - 0.4) * 100 + 25
+    y = (int(position[1]) - 1.5) * 100 + 25
+    return x, y
 
 
-def computer_move():
-    global turn
-    if turn == 'white':  # Ход белых фишек (компьютер)
-        available_positions = [pos for pos, piece in pieces.items() if piece is None]
-        if available_positions:
-            position = random.choice(available_positions)
-            pieces[position] = 'white'
-            turn = 'black'
+# Функция для отрисовки игрового поля
+def draw_board():
+    screen.blit(game_board_image, (0, 0))
+    draw_points()
+    for position, piece in pieces.items():
+        if piece == 'black':
+            screen.blit(black_piece_image, (position_to_pixel(position)))
+        elif piece == 'white':
+            screen.blit(white_piece_image, (position_to_pixel(position)))
+
+# Функция отрисовки линии мельницы
+def draw_mill_line(positions):
+    pygame.draw.line(screen, (255, 0, 0), position_to_pixel(positions[0]), position_to_pixel(positions[1]), 2)
+
+
+# Обработка кликов мыши для расстановки фишек
+def handle_player_click_human(mouse_position):
+    # Добавление счетчиков фишек для каждого игрока
+    black_pieces_count = 0
+    white_pieces_count = 0
+
+    pixel_position = mouse_position
+    for pos, (x, y) in points.items():
+        if x - 15 < pixel_position[0] < x + 15 and y - 15 < pixel_position[1] < y + 15:
+            if pieces[pos] is None and (turn == 'black' and black_pieces_count < 9 or turn == 'white' and white_pieces_count < 9):
+                pieces[pos] = turn
+                if turn == 'black':
+                    black_pieces_count += 1
+                else:
+                    white_pieces_count += 1
+                change_turn()
 
 
 
 
-def check_mill(position):
+def get_mill_positions(position):
     # Получаем индексы строки и столбца из позиции
     row = int(position[1]) - 1
     col = ord(position[0]) - ord('A')
 
-    # Вертикальные линии
-    if pieces['A1'] == pieces['A4'] == pieces['A7'] or \
-            pieces['B2'] == pieces['B4'] == pieces['B6'] or \
-            pieces['C3'] == pieces['C4'] == pieces['C5'] or \
-            pieces['D1'] == pieces['D2'] == pieces['D3'] or \
-            pieces['D5'] == pieces['D6'] == pieces['D7'] or \
-            pieces['E3'] == pieces['E4'] == pieces['E5'] or \
-            pieces['F2'] == pieces['F4'] == pieces['F6'] or \
-            pieces['G1'] == pieces['G4'] == pieces['G7']:
-        return True
-
     # Горизонтальные линии
-    if pieces['A1'] == pieces['D1'] == pieces['G1'] or \
-            pieces['B2'] == pieces['D2'] == pieces['F2'] or \
-            pieces['C3'] == pieces['D3'] == pieces['E3'] or \
-            pieces['A4'] == pieces['B4'] == pieces['C4'] or \
-            pieces['E4'] == pieces['F4'] == pieces['G4'] or \
-            pieces['C5'] == pieces['D5'] == pieces['E5'] or \
-            pieces['B6'] == pieces['D6'] == pieces['F6'] or \
-            pieces['A7'] == pieces['D7'] == pieces['G7']:
-        return True
+    if pieces[f'A{row + 1}'] == pieces[f'B{row + 1}'] == pieces[f'C{row + 1}']:
+        return [f'A{row + 1}', f'B{row + 1}', f'C{row + 1}']
+    elif pieces[f'D{row + 1}'] == pieces[f'E{row + 1}'] == pieces[f'F{row + 1}']:
+        return [f'D{row + 1}', f'E{row + 1}', f'F{row + 1}']
+    elif pieces[f'G{row + 1}'] == pieces[f'A{row + 1}'] == pieces[f'D{row + 1}']:
+        return [f'G{row + 1}', f'A{row + 1}', f'D{row + 1}']
+    elif pieces[f'B{row + 1}'] == pieces[f'D{row + 1}'] == pieces[f'F{row + 1}']:
+        return [f'B{row + 1}', f'D{row + 1}', f'F{row + 1}']
 
-    return False
+    # Вертикальные линии
+    if pieces[f'{chr(col + ord("A"))}1'] == pieces[f'{chr(col + ord("A"))}2'] == pieces[f'{chr(col + ord("A"))}3']:
+        return [f'{chr(col + ord("A"))}1', f'{chr(col + ord("A"))}2', f'{chr(col + ord("A"))}3']
+    elif pieces[f'{chr(col + ord("A"))}4'] == pieces[f'{chr(col + ord("A"))}5'] == pieces[f'{chr(col + ord("A"))}6']:
+        return [f'{chr(col + ord("A"))}4', f'{chr(col + ord("A"))}5', f'{chr(col + ord("A"))}6']
+    elif pieces[f'{chr(col + ord("A"))}7'] == pieces[f'{chr(col + ord("A"))}4'] == pieces[f'{chr(col + ord("A"))}1']:
+        return [f'{chr(col + ord("A"))}7', f'{chr(col + ord("A"))}4', f'{chr(col + ord("A"))}1']
 
-
-def is_valid_move(start, end):
-    # Получаем индексы строки и столбца из начальной и конечной позиции
-    start_row = int(start[1]) - 1
-    start_col = ord(start[0]) - ord('A')
-    end_row = int(end[1]) - 1
-    end_col = ord(end[0]) - ord('A')
-
-    # Проверка допустимости хода (возможно, вам потребуется адаптировать эту логику под вашу игру)
-    if pieces[start] == turn and pieces[end] is None:
-        # Перемещение на соседнюю клетку
-        if abs(start_row - end_row) + abs(start_col - end_col) == 1:
-            return True
-        # Перемещение на клетку по диагонали
-        elif abs(start_row - end_row) == 1 and abs(start_col - end_col) == 1:
-            return True
-        # Если фишка принадлежит текущему игроку, можно "прыгнуть" через соседнюю фишку
-        elif pieces[start] == turn and pieces[end] is None and jump_over_adjacent(start, end):
-            return True
-
-    return False
+    return []
 
 
-def jump_over_adjacent(start, end):
-    # Проверка, можно ли "прыгнуть" через соседнюю фишку
-    start_row = int(start[1]) - 1
-    start_col = ord(start[0]) - ord('A')
-    end_row = int(end[1]) - 1
-    end_col = ord(end[0]) - ord('A')
 
-    # Проверка, что начальная и конечная клетки соседние
-    if abs(start_row - end_row) + abs(start_col - end_col) != 2:
-        return False
+def setup_pieces_human():
+    global turn, player_turn
 
-    # Определение координат соседней клетки
-    adjacent_row = (start_row + end_row) // 2
-    adjacent_col = (start_col + end_col) // 2
-    adjacent_position = f'{chr(adjacent_col + ord("A"))}{adjacent_row + 1}'
+    player_turn = True
+    screen.fill(WHITE)
+    draw_board()
+    pygame.display.flip()
 
-    # Проверка, что соседняя клетка занята фишкой
-    return pieces[adjacent_position] and pieces[adjacent_position] != turn
-
-
-# Обработка кликов мыши
-def handle_click(pixel_position):
-
-    position = None
-    for pos, pixel_pos in pieces.items():
-        if pixel_pos[0] < mouse_position[0] < pixel_pos[0] + black_piece_image.get_width() and \
-                pixel_pos[1] < mouse_position[1] < pixel_pos[1] + black_piece_image.get_height():
-            position = pos
-            break
-
-    # if position:
-    #     handle_click(pixel_position)
-
-
-def handle_game_events():
-    global running
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_position = pygame.mouse.get_pos()
-            if player_turn:
-                handle_player_click(mouse_position)
-                if game_mode == 'vs_computer' and player_turn:
-                    computer_move()
+    while any(piece is None for piece in pieces.values()):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_position = pygame.mouse.get_pos()
+                pixel_position = mouse_position
+                x, y = pixel_position
+                letters = 'ABCDEFG'
+                position = letters[x // 100] + str(y // 100 + 1)
+                if pieces[position] is None:
+                    pieces[position] = turn
+                    draw_board()
+                    pygame.display.flip()
                     change_turn()
-
-
-def handle_player_click(mouse_position):
-    x, y = mouse_position
-    letters = 'ABCDEFG'
-    position = letters[x // 100] + str(y // 100 + 1)
-    handle_click(position)
+                    print_board()  # Выводим текущее состояние доски
 
 
 def main_game_loop(game_mode):
+    if game_mode == 'two_players':
+        setup_pieces_human()
+    elif game_mode == 'vs_computer':
+        pass
+
     global player_turn
-
-    player_turn = True  # Начинаем с хода первого игрока
-    running = True
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_position = pygame.mouse.get_pos()
-                if player_turn:
-                    handle_player_click(mouse_position)
-                    if game_mode == 'vs_computer' and player_turn:  # Если игрок против компьютера, ход компьютера
-                        computer_move()
-                        change_turn()
-
-        screen.fill(WHITE)
-        draw_board()
-        pygame.display.flip()
-
-        if check_win():
-            print(f'Игрок {turn} выиграл!')
-            pygame.time.delay(2000)
-            running = False
-
-        pygame.time.Clock().tick(30)
-
 
 def change_turn():
     global turn
     turn = 'black' if turn == 'white' else 'white'
 
 
-running = True
-game_mode = None  # Режим игры: 'two_players' или 'vs_computer'
-
-start_screen_buttons = draw_start_screen()
-
-while running and game_mode is None:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_position = pygame.mouse.get_pos()
-            game_mode = handle_start_screen_click(mouse_position, *start_screen_buttons)
+def print_board():
+    for row in range(1, 8):
+        for col in 'ABCDEFG':
+            position = f"{col}{row}"
+            piece = pieces.get(position, None)
+            print(piece or '.', end=' ')
+        print()
 
 
-    pygame.display.flip()
+# Запуск программы
+if __name__ == "__main__":
+    running = True
+    game_mode = None  # Режим игры: 'two_players' или 'vs_computer'
 
-if game_mode:
-    main_game_loop(game_mode)
+    start_screen_buttons = draw_start_screen()
 
-pygame.quit()
-sys.exit()
+    while running and game_mode is None:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_position = pygame.mouse.get_pos()
+                game_mode = handle_start_screen_click(mouse_position, *start_screen_buttons)
+
+        pygame.display.flip()
+
+    if game_mode:
+        main_game_loop(game_mode)
+
+    pygame.quit()
+    sys.exit()
